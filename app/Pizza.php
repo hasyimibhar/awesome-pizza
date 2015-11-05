85d172cc3b917883450e597d056d7decb79880d1
Hasyimi Bahrudin <hasyimi@terato.com>
2015-11-06 00:01:14 +0800

    Add Pizza model

---
+++ app/Pizza.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AwesomePizza;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pizza extends Model
+{
+    //
+}
---
+++ database/migrations/2015_11_05_160008_create_pizzas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePizzasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('price')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pizzas');
+    }
+}
