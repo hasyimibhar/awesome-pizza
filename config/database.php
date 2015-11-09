dbb537a0d2c812a5b24b82a767b67b05c51c2180
Hasyimi Bahrudin <hasyimi@terato.com>
2015-11-10 02:30:19 +0800

    Enable mysql for travis

--- config/database.php
+++ config/database.php
@@ -64,6 +64,18 @@ return [
             'strict'    => false,
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE_TESTING', 'forge'),
+            'username'  => env('DB_USERNAME_TESTING', 'forge'),
+            'password'  => env('DB_PASSWORD_TESTING', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
---
+++ tests/integration/PizzaRepositoryTest.php
@@ -0,0 +1,115 @@
+<?php
+
+use AwesomePizza\Menu\PizzaRepository;
+
+class PizzaRepositoryTest extends \Codeception\TestCase\Test
+{
+    use Codeception\Specify;
+
+    /**
+     * @var \IntegrationTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+        $this->specifyConfig()->deepClone(false);
+
+        // Run migrations
+        $artisan = $this->tester->grabService('Illuminate\Contracts\Console\Kernel');
+        $artisan->call('migrate', ['--database' => 'testing']);
+    }
+
+    public function testAllWithNoPizza()
+    {
+        $this->specify("all should return no pizzas", function() {
+            $repository = new PizzaRepository($this->getDatabaseConnection());
+            $pizzas = $repository->all();
+
+            verify($pizzas)->notNull();
+            verify($pizzas->count())->equals(0);
+        });
+    }
+
+    public function testAllWithOnePizza()
+    {
+        $this->specify("all should return one pizza", function() {
+            $now = date('Y-m-d H:i:s');
+            $this->tester->haveInDatabase('pizzas', [
+                'id' => 1,
+                'name' => 'Awesome Pizza',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+
+            $repository = new PizzaRepository($this->getDatabaseConnection());
+            $pizzas = $repository->all();
+
+            verify($pizzas)->notNull();
+            verify($pizzas->count())->equals(1);
+
+            $pizza = $pizzas->first();
+            verify($pizza->id)->equals(1);
+            verify($pizza->name)->equals('Awesome Pizza');
+            verify($pizza->created_at)->equals($now);
+            verify($pizza->updated_at)->equals($now);
+        });
+    }
+
+    public function testAllWithManyPizzas()
+    {
+        $this->specify("all should return many pizzas", function() {
+            $now = date('Y-m-d H:i:s');
+
+            $this->tester->haveInDatabase('pizzas', [
+                'id' => 1,
+                'name' => 'Awesome Pizza',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+
+            $this->tester->haveInDatabase('pizzas', [
+                'id' => 2,
+                'name' => 'Cool Pizza',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+
+            $this->tester->haveInDatabase('pizzas', [
+                'id' => 3,
+                'name' => 'Epic Pizza',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+
+            $repository = new PizzaRepository($this->getDatabaseConnection());
+            $pizzas = $repository->all();
+
+            verify($pizzas)->notNull();
+            verify($pizzas->count())->equals(3);
+
+            $pizza1 = $pizzas[0];
+            verify($pizza1->id)->equals(1);
+            verify($pizza1->name)->equals('Awesome Pizza');
+            verify($pizza1->created_at)->equals($now);
+            verify($pizza1->updated_at)->equals($now);
+
+            $pizza2 = $pizzas[1];
+            verify($pizza2->id)->equals(2);
+            verify($pizza2->name)->equals('Cool Pizza');
+            verify($pizza2->created_at)->equals($now);
+            verify($pizza2->updated_at)->equals($now);
+
+            $pizza3 = $pizzas[2];
+            verify($pizza3->id)->equals(3);
+            verify($pizza3->name)->equals('Epic Pizza');
+            verify($pizza3->created_at)->equals($now);
+            verify($pizza3->updated_at)->equals($now);
+        });
+    }
+
+    protected function getDatabaseConnection()
+    {
+        return $this->tester->grabService('Illuminate\Database\ConnectionInterface');
+    }
+}
